6f58893e7e42145dd3ffb22e28054332beff5adc
Jonatan Samoocha <jsamoocha@xebia.com>
2015-09-03 16:14:14 +0200

    Temporary spelling-and-grammar-nazi-mode

--- start-playbook.sh
+++ start-playbook.sh
@@ -9,4 +9,4 @@ else
   ansible-playbook -i hosts local-cdh5.yml  --extra-vars install_dir=${INSTALL_DIR}
 fi
 
-echo -e "Installation off CDH Finished \n\nTo use local mode execute:\n  \$ source ${INSTALL_DIR}/.bash_profile_hadoop\n  \$ switch_local_cdh5\n\nTo use pseudo distributed mode:\n  \$ source ${INSTALL_DIR}/.bash_profile_hadoop\n  \$ switch_pseudo_cdh5\n  \$ hadoop namenode -format (only first time!!)\n  \$ start-dfs.sh\n  \$ start-yarn.sh\n\nTo stop:\n  \$ stop-yarn.sh\n  \$ stop-dfs.sh\n"
+echo -e "Installation of CDH Finished \n\nTo use local mode execute:\n  \$ source ${INSTALL_DIR}/.bash_profile_hadoop\n  \$ switch_local_cdh5\n\nTo use pseudo distributed mode:\n  \$ source ${INSTALL_DIR}/.bash_profile_hadoop\n  \$ switch_pseudo_cdh5\n  \$ hadoop namenode -format (only first time!!)\n  \$ start-dfs.sh\n  \$ start-yarn.sh\n\nTo stop:\n  \$ stop-yarn.sh\n  \$ stop-dfs.sh\n"
