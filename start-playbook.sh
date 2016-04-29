6e1ca58bd00bd2f8228573ff8a983870cf213a6f
Kris Geusebroek <kgeusebroek@KgMBP2015.local>
2016-04-29 13:41:55 +0200

    Expand ~ in input pat to $HOME to prevent "java.lang.IllegalArgumentException: URI has an authority component" in name node startup due to ~ in config parameters

--- start-playbook.sh
+++ start-playbook.sh
@@ -6,7 +6,7 @@ if [ -z "${INSTALL_DIR}" ]; then
   INSTALL_DIR=/tmp/hadoop/cdh5
   ansible-playbook -i hosts local-cdh5.yml
 else
-  ansible-playbook -i hosts local-cdh5.yml  --extra-vars install_dir=${INSTALL_DIR}
+  ansible-playbook -i hosts local-cdh5.yml  --extra-vars install_dir=${INSTALL_DIR/#\~/$HOME}
 fi
 
 echo -e "Installation of CDH Finished \n\nTo use local mode execute:\n  \$ source ${INSTALL_DIR}/.bash_profile_hadoop\n  \$ switch_local_cdh5\n\nTo use pseudo distributed mode:\n  \$ source ${INSTALL_DIR}/.bash_profile_hadoop\n  \$ switch_pseudo_cdh5\n  \$ hadoop namenode -format (only first time!!)\n  \$ start-dfs.sh\n  \$ start-yarn.sh\n\nTo stop:\n  \$ stop-yarn.sh\n  \$ stop-dfs.sh\n"
